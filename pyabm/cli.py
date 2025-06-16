56f0d47b16b9aebf4302c44538819ab67fbcf182
Kain <26943220+kaincenteno@users.noreply.github.com>
2025-06-15 23:26:36 -0700

    Add support for listing devices in a specified MDM server in CLI

--- pyabm/cli.py
+++ pyabm/cli.py
@@ -16,10 +16,21 @@ def list_mdm_servers():
     for server in servers:
         print(server)
 
+def list_devices_in_mdm_server():
+    if len(sys.argv) < 3:
+        print("Usage: python cli.py mdm_server <server_id>")
+        print("You can get the server_id from the 'mdm_servers' command.")
+        exit(1)
+    server_id = sys.argv[2]
+    client = Client()
+    devices = client.list_devices_in_mdm_server(server_id)
+    for device in devices:
+        print(device)
+
 def main():
     if not len(sys.argv) > 1:
         print("Usage: python cli.py <command> [<args>]")
-        print("Available commands: device mdm_servers")
+        print("Available commands: device mdm_servers mdm_server")
         exit(1)
 
     match sys.argv[1]:
@@ -27,8 +38,11 @@ def main():
             query_device()
         case "mdm_servers":
             list_mdm_servers()
+        case "mdm_server":
+            list_devices_in_mdm_server()
         case _:
-            print("Invalid command. Available commands: device mdm_servers")
+            print("Invalid command.")
+            print("Available commands: device mdm_servers mdm_server")
             exit(1)
 
 if __name__ == "__main__":
