2a1061aac6ed5162f01dc79a50bd94de45c691c8
Kain <26943220+kaincenteno@users.noreply.github.com>
2025-06-16 22:46:10 -0700

    Implement retry logic for device listing to handle status code that are not 200 for listing

--- pyabm/abm_requests.py
+++ pyabm/abm_requests.py
@@ -1,5 +1,6 @@
 import requests
 from pyabm.models import OrgDeviceResponse, MdmServersResponse, MdmServerDevicesLinkagesResponse, OrgDevicesResponse
+import time
 
 # creating a session to reuse connections
 # didn't really improved performance, should revert back 
@@ -43,13 +44,27 @@ def list_devices(access_token, next=None) -> OrgDevicesResponse:
     if next:
         url = next
     else:
+        # Default is 100 devices. Documentation says max is 1000 but 
+        # currently querying a unspecified number returns status code 500
+        # increase this when apple fixes the issue to 1000
         url = 'https://api-business.apple.com/v1/orgDevices'
-    response = session.get(url, headers=_auth_headers(access_token))
 
-    if response.status_code == 200:
-        return OrgDevicesResponse.model_validate(response.json())
-    else:
-        response.raise_for_status()
+    retries = 3
+    for attempt in range(retries):
+        response = session.get(url, headers=_auth_headers(access_token))
+
+        # ABM has been returning 500, this is a workaround to retry 2 times
+        # before raising an error.
+        if response.status_code != 200:
+            if attempt < retries - 1:
+                print(response.status_code, 'sleeping')
+                time.sleep(5)
+                continue
+            else:
+                print(response.text)
+                response.raise_for_status()   
+        else:
+            return OrgDevicesResponse.model_validate(response.json())
 
 def get_device(device_id, access_token) -> OrgDeviceResponse:
     """
@@ -92,7 +107,6 @@ def list_devices_in_mdm_server(server_id: str, access_token, next=None) -> MdmSe
     :param next: Optional; the URL for the next page of results.
     :return: An MdmServerResponse object containing the MDM server information.
     """
-    import time
     if next:
         url = next
     else:
@@ -102,15 +116,14 @@ def list_devices_in_mdm_server(server_id: str, access_token, next=None) -> MdmSe
     for attempt in range(retries):
         response = session.get(url, headers=_auth_headers(access_token))
 
-        if response.status_code == 200:
-            return MdmServerDevicesLinkagesResponse.model_validate(response.json())
-        
-        if response.status_code == 429:
+        # ABM has been returning 500, this is a workaround to retry 2 times
+        # before raising an error.
+        if response.status_code != 200:
             if attempt < retries - 1:
                 time.sleep(5)
                 continue
             else:
+                print(response.text)
                 response.raise_for_status()
-
         else:
-            response.raise_for_status()
+            return MdmServerDevicesLinkagesResponse.model_validate(response.json())
